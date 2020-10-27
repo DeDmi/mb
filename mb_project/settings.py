60682ef36d78e3536b1e63b6060017bd4aa13668
dzmitry <v.dmitryvasilyev@gmail.com>
2020-10-27 15:55:58 +0200

    te

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ppqii%1@20j^#l!#pna%6(nx2$l6s^$i7+-=evua-=hncb*2-@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
